2e1bb50bab4434ab8d0f85705440b084de303067
Max <max.derkach23@gmail.com>
2024-12-08 19:18:46 +0100

    fix

--- src/server.js
+++ src/server.js
@@ -2,16 +2,14 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import env from './utils/env.js';
-import dotenv from 'dotenv';
 
-import { errorHandler } from './middlewares/errorHandlers.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import rootRouter from './routes/index.js';
+import rootRouter from './routers/index.js';
 import cookieParser from 'cookie-parser';
-import { UPLOAD_DIR } from './constants/index.js';
+import { UPLOAD_DIR } from './constants/constants.js';
 
 export default function setupServer() {
-  dotenv.config();
   const PORT = Number(env('PORT', 3000));
   const app = express();
 
